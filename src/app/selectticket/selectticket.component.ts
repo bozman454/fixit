9e3d572be3ba727ce0e8dfe175fb52b1f79d1cd3
Willem <bosman.willem@tcs.com>
2019-08-14 10:10:43 -0400

    minor tweaks to styling and what not

--- src/app/selectticket/selectticket.component.ts
+++ src/app/selectticket/selectticket.component.ts
@@ -3,18 +3,19 @@ import { ActivatedRoute } from '@angular/router';
 import { GetticketService } from '../getticket.service';
 import { Router } from '@angular/router';
 import { HttpHeaders } from '@angular/common/http';
+import { Ticket } from '../ticket';
 @Component({
   selector: 'app-selectticket',
   templateUrl: './selectticket.component.html',
   styleUrls: ['./selectticket.component.css']
 })
 export class SelectticketComponent implements OnInit {
-  tickets = [];
+  tickets: Ticket [];
   selectedticket;
   selected = {};
   edittick: boolean;
 
-  constructor(private router: Router, private getticketservice: GetticketService, private route: ActivatedRoute) { 
+  constructor(private router: Router, private getticketservice: GetticketService, private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       this.selectedticket = params.ticket_id;
     });
@@ -29,15 +30,15 @@ export class SelectticketComponent implements OnInit {
   }
 
   submitedit(id, name, tech, model, status, notes){
-    
+
     var out_name = name;
     if(out_name.length < 1)
       out_name = this.selected.customer_name;
-    
+
     var out_tech = tech;
     if(out_tech.length < 1)
       out_tech = this.selected.assigned_technician;
-    
+
     var out_model = model;
     if(out_model.length < 1)
       out_model = this.selected.device_model;
@@ -49,9 +50,9 @@ export class SelectticketComponent implements OnInit {
     if(out_notes.length < 1)
       out_notes = this.selected.device_notes;
 
-    
 
-    
+
+
     const out = {
       customer_name: out_name,
       assigned_technician: out_tech,
@@ -65,7 +66,7 @@ export class SelectticketComponent implements OnInit {
     .subscribe(res =>{
       if(res.success == 'true')
         this.router.navigateByUrl('/dashboard')
-      
+
     },
     err => console.log(err));
 
@@ -78,9 +79,9 @@ export class SelectticketComponent implements OnInit {
           this.tickets = res.rows;
           this.tickets.forEach(element => {
             if(element.ticket_id.toString() === this.selectedticket)
-            
+
             this.selected = element;
-          
+
             console.log(element.ticket_id);
           });
         }
@@ -88,7 +89,7 @@ export class SelectticketComponent implements OnInit {
       , err => console.log(err));
     this.edittick = false;
     }
-  
+
 
   deleteticket(id){
     this.getticketservice.deleteTickets(id)
---
+++ src/app/ticket.ts
@@ -0,0 +1,8 @@
+export class Ticket{
+  ticket_id: number;
+  customer_name: string;
+  assigned_technician: string;
+  device_model: string;
+  device_status: string;
+  device_notes: string;
+}
